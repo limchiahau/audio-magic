66cd2981d680ac0b0a40f2555255af36a67386fa
Lim Chia Hau <chl4source@gmail.com>
2018-10-29 11:59:15 +0800

    Refactored the code into the OOP style. Now there are 3 main classes.
    1) Main (main program)
    2) AudioManager
    3) PacMD (singleton)
    
    The objects communicate in this way:
    
    Main <=> AudioManager <=> PacMD

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,163 +1,54 @@
 package com.chl.audiomagic
 
+
 fun main(arg: Array<String>) {
-    val outputs = getListOfAudioOutput()
-
-    //do nothing if there are no audio output devices
-    if (outputs.isNotEmpty()) {
-        switchAudioOutput(
-                getAudioInput(),
-                getEnabledAudioOutput(outputs),
-                getPrioritizedAudioOutput(outputs)
-        )
-    }
-}
-
-///////////////////////////////////////////////////////////////////FUNCTIONS
-
-
-fun getListOfAudioOutput(): List<AudioOutput> {
-    val rawAudioOut = bash("pacmd list-sinks")
-
-    //list of regexp to use against rawAudioOut
-    return listOf(
-            "index: [0-9]+",    //ID
-            "name: .+",         //Name
-            "priority [0-9]+",  //Priority
-            ". index: [0-9]+"   //Enabled
-
-    ).map {
-        it.regexp(rawAudioOut)
-
-    }.let {
-        it.toRows()
-
-    }.map {
-        AudioOutput(
-                id = it.get(0).parseId(),
-                name = it.get(1).parseName(),
-                priority = it.get(2).parsePriority(),
-                enabled = it.get(3).parseEnabled()
-        )
-
-    }
-}
-
-
-fun getPrioritizedAudioOutput(list: List<AudioOutput>): AudioOutput {
-    return list
-            .asSequence() //suggested by ide
-            .sortedBy(AudioOutput::priority)
-            .last()
-}
-
-
-fun getEnabledAudioOutput(list: List<AudioOutput>): AudioOutput {
-    val device = list.find(AudioOutput::enabled)
-    if (device == null) {
-        throw Exception("No audio output device found")
-    } else {
-        return device
-    }
-}
-
-
-fun getAudioInput(): AudioInput {
-    val rawAudioIn = bash("pacmd list-sink-inputs")
-
-    return "index: [0-9]+"
-            .regexp(rawAudioIn)
-            .map(String::parseId)
-            .let(::AudioInput)
-}
-
-
-////////////////////////////////////////////////////////////////////////EXTENSIONS
-
-
-fun String.parseId(): Int {
-    return split(" ")
-            .last()
-            .toInt()
-}
-
-
-fun String.parseName(): String {
-    return split(" ")
-            .last()
-            .drop(1) //remove the "<" in front
-            .dropLast(1) //"remove the ">" from the back
-}
-
-
-fun String.parsePriority(): Int {
-    return split(" ")
-            .last()
-            .toInt()
-}
-
-
-//indicates if the audio device is enabled or not
-//in the data, enabled audio output devices will have a * as the prefix
-fun String.parseEnabled(): Boolean {
-    return contains('*')
-}
-
-
-fun String.regexp(match: String): List<String> {
-    return toRegex()
-            .findAll(match)
-            .toList()
-            .map(MatchResult::value)
+    val manager = AudioManager()
+    manager.autoSwitchOutput()
 }
 
 
 /////////////////////////////////////////////////////////////////////////////CLASS
 
 
-fun switchAudioOutput(audioInput: AudioInput,
-                      enabledAudioOutput: AudioOutput,
-                      prioritizedAudioOutput: AudioOutput) {
+class AudioManager {
 
-    fun switchAudioOutputTo(audioOut: AudioOutput) {
-        audioInput.setAudioOutputDeviceTo(audioOut)
+    //there is no need to switch if the number of outputs are lower than 2
+    //1) if there are no outputs there is nothing to switch to.
+    //2) if there is 1 output it will automatically be of the highest priority.
+    fun autoSwitchOutput() {
+        if (hasOutput(2)) autoSwitch()
     }
 
-    fun isNotPrioritized(audioOut: AudioOutput): Boolean
-            = audioOut != prioritizedAudioOutput
+    private fun hasOutput(numOutputs: Int): Boolean {
+        return PacMD.getAudioOutputs().size >= numOutputs
+    }
 
-    if (isNotPrioritized(enabledAudioOutput))
-        switchAudioOutputTo(prioritizedAudioOutput)
+    private fun autoSwitch() {
+        val allOutput = PacMD.getAudioOutputs()
+        val enabledOutput = getEnabledAudioOutput(allOutput)
+        val prioritizedOutput = getPrioritizedAudioOutput(allOutput)
+
+        if (enabledOutput != prioritizedOutput)
+            PacMD.setAudioOutputDeviceTo(prioritizedOutput)
+    }
+
+    private fun getPrioritizedAudioOutput(list: List<AudioOutput>): AudioOutput {
+        return list
+                .asSequence() //suggested by ide
+                .sortedBy(AudioOutput::priority)
+                .last()
+    }
+
+    private fun getEnabledAudioOutput(list: List<AudioOutput>): AudioOutput? {
+        return list.find(AudioOutput::enabled)
+    }
 }
 
 
 data class AudioOutput(val name: String, val id: Int, val priority: Int, val enabled: Boolean)
 
 
-//use id default if there are no available sink inputs
-class AudioInput(private val ids: List<Int>) {
-    fun setAudioOutputDeviceTo(audioOut: AudioOutput) {
-        setDefaultSink(audioOut)
-        moveSinkInput(audioOut)
-    }
-
-    private fun moveSinkInput(audioOut: AudioOutput) {
-        if (thereAreActiveSinkInputs()) {
-            ids.forEach {
-                bash("pacmd move-sink-input $it ${audioOut.id}")
-            }
-        }
-    }
-
-    private fun thereAreActiveSinkInputs(): Boolean {
-        return ids.isNotEmpty()
-    }
-
-    private fun setDefaultSink(audioOut: AudioOutput) {
-        println("Setting default audio output to: ${audioOut.name}")
-        bash("pacmd set-default-sink ${audioOut.name}")
-    }
-}
+data class AudioInput(val id: Int)
 
 
 typealias Grid = List<List<String>>
@@ -196,6 +87,92 @@ fun Grid.dropAll(): Grid {
 fun Grid.dropFirstRow(): Grid {
     return map {
         it.drop(1)
+    }
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////SINGLETON
+
+
+object PacMD {
+    fun getAudioOutputs(): List<AudioOutput> {
+        val rawAudioOut = bash("pacmd list-sinks")
+
+        //list of regexp to use against rawAudioOut
+        return listOf(
+            "index: [0-9]+",    //ID
+            "name: .+",         //Name
+            "priority [0-9]+",  //Priority
+            ". index: [0-9]+"   //Enabled
+
+        ).map {
+            it.regexp(rawAudioOut)
+
+        }.toRows().map {
+            AudioOutput(
+                    id = it.get(0).parseId(),
+                    name = it.get(1).parseName(),
+                    priority = it.get(2).parsePriority(),
+                    enabled = it.get(3).parseEnabled()
+            )
+
+        }
+    }
+
+    fun setAudioOutputDeviceTo(output: AudioOutput) {
+        setDefaultSink(output)
+        getAudioInputs().forEach {
+            moveSinkInput(it, output)
+        }
+    }
+
+    private fun getAudioInputs(): List<AudioInput> {
+        val rawAudioIn = bash("pacmd list-sink-inputs")
+
+        return "index: [0-9]+"
+                .regexp(rawAudioIn)
+                .map({it.parseId()})
+                .map(::AudioInput)
+    }
+
+    private fun moveSinkInput(input: AudioInput, output: AudioOutput) {
+        bash("pacmd move-sink-input ${input.id} ${output.id}")
+    }
+
+    private fun setDefaultSink(output: AudioOutput) {
+        bash("pacmd set-default-sink ${output.name}")
+    }
+
+    private fun String.parseId(): Int {
+        return split(" ")
+                .last()
+                .toInt()
+    }
+
+    private fun String.parseName(): String {
+        return split(" ")
+                .last()
+                .drop(1) //remove the "<" in front
+                .dropLast(1) //"remove the ">" from the back
+    }
+
+    private fun String.parsePriority(): Int {
+        return split(" ")
+                .last()
+                .toInt()
+    }
+
+    //indicates if the audio device is enabled or not
+    //in the data, enabled audio output devices will have a * as the prefix
+    private fun String.parseEnabled(): Boolean {
+        return contains('*')
+    }
+
+    private fun String.regexp(match: String): List<String> {
+        return toRegex()
+                .findAll(match)
+                .toList()
+                .map(MatchResult::value)
     }
 }
 
